5f5e8c03f2f0e8097bc80a16456c4a171de8ae5f
José María Caballero Alba <caballeroalba@gmail.com>
2015-03-27 13:19:42 +0100

    Añadida la opción de crear reglas desde los detalles de una cadena (sin funcionalidad aún)

--- src/main.c
+++ src/main.c
@@ -269,18 +269,19 @@ void list_chain_details(int ntable, int nchain, struct table_list *list)
 	const char *hook=nftables_gui_chain_attr_get_str(ch,
 							NFTABLES_GUI_CHAIN_ATTR_HOOK);
 
-	char *opts[4];
+	char *opts[5];
 	opts[1]=strdup(chain_name);
 	opts[2]=strdup(hook);
-	opts[3]="List rules";
-    opts[4]="Back";	
+    opts[3]="Create rule";
+	opts[4]="List rules";
+    opts[5]="Back";	
 	char *message="You are in ";
 	const char* table_name=nftables_gui_table_attr_get_str(t,
 							NFTABLES_GUI_TABLE_ATTR_TABLE_NAME);
 	//message=strcat(message,table_name);
 	char *message2=" chain ";
 	//message=strcat(message,strcat(message2,chain_name));
-	int result=print_menu(1,opts,5,"","");
+	int result=print_menu(1,opts,6,"","");
 }
 
 struct chain * get_chain(struct table *t, int nchain){
